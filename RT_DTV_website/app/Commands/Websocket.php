5144c6f73525fd4fa86b09841540ab631f8b499f
max2124 <max93.01.11@gmail.com>
2025-09-28 08:40:06 +0800

    modified Websocket.php

--- RT_DTV_website/app/Commands/Websocket.php
+++ RT_DTV_website/app/Commands/Websocket.php
@@ -13,12 +13,11 @@ class Websocket extends BaseCommand
 
     public function run(array $params)
     {
-        $python_path = "C:\\Users\\User20240709\\AppData\\Local\\Programs\\Python\\Python313\\python.exe";  // 這邊填入PYTHON環境執行黨
-        $command =  $python_path . ' ' . FCPATH . 'python/website.py';  // 指向 Python 腳本
+        $python_path = 'C:\Users\vicky\anaconda3\envs\pj11\python.exe'; // 這邊填入PYTHON環境執行黨
+        $command =  $python_path . ' ' . FCPATH . 'python/car_track.py';  // 指向 Python 腳本
         $output = shell_exec($command);
 
         CLI::write("WebSocket 伺服器啟動: " . $output, 'green');
 
     }
 }
-// php -S 0.0.0.0:8081 -t public
