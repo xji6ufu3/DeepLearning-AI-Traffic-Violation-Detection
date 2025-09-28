6377026f3fc19f8ab0b065b11c4b9a1074c50e0c
max2124 <max93.01.11@gmail.com>
2025-09-28 08:46:48 +0800

    modified Home.php

--- RT_DTV_website/app/Controllers/Home.php
+++ RT_DTV_website/app/Controllers/Home.php
@@ -2,11 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
-use App\Models\VideoModel;
-use App\Models\RoadModel;
-
 class Home extends BaseController
 {
     public function index(): string
